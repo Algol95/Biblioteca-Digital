70c125891aa002ed583d3cf76bb4ff3d03830043
Algol95 <angelaragon.developer@gmail.com>
2025-02-26 13:01:21 +0100

    Created view modal book

--- src/services/printService.js
+++ src/services/printService.js
@@ -1,4 +1,5 @@
 import { initPopovers } from "../utils/popover.js";
+import { initModals } from "../utils/modal.js";
 
 /**
  * Dirección URL para consultas con la API
@@ -10,44 +11,55 @@ const grid = document.querySelector("#books-grid");
 
 /**
  *  Constante que alberga el elemento con la ID #tableBooks
- * @type {document.getElementById} 
+ * @type {document.getElementById}
  * @author {Ángel Aragón}
  */
 const tableAdmin = document.getElementById("tableBooks");
 
+/**
+ * Constante que alberga el elemento con la ID #modalBody
+ * @type {document.getElementById}
+ */
+const modalBody = document.getElementById("modalBody");
+
+/**
+ * Constante que alber el elemento con la ID #modalLabel
+ *
+ * @type {document.getElementById}
+ */
+const modalLabel = document.getElementById("modalLabel");
 
 /**
  * Constante que Boolean que es verdadero o falso si se encuentra en la página de admin o no.
  * @type {boolean}
  * @author {Ángel Aragón}
- */ 
-const isAdminPage = window.location.pathname.includes("admin.html")
-
+ */
+const isAdminPage = window.location.pathname.includes("admin.html");
 
 //----------------GET FUNCTION----------------//
 
 //READ - Get All the Books
 export async function getAllBooks() {
-    try {
-        const res = await fetch(API_URL);
-        if (!res.ok) throw new Error(`Error: ${res.status}`);
-        const books = await res.json();
-        return books;
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-    }
+  try {
+    const res = await fetch(API_URL);
+    if (!res.ok) throw new Error(`Error: ${res.status}`);
+    const books = await res.json();
+    return books;
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
 }
 
 // READ - Get only one Book
 async function getSingleBook(id) {
-    try {
-        const res = await fetch(`${API_URL}/${id}`);
-        if (!res.ok) throw new Error(`Error: ${res.status}`);
-        const book = await res.json();
-        return book;
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-    }
+  try {
+    const res = await fetch(`${API_URL}/${id}`);
+    if (!res.ok) throw new Error(`Error: ${res.status}`);
+    const book = await res.json();
+    return book;
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
 }
 
 //----------------PRINT FUNCTION----------------//
@@ -56,24 +68,24 @@ async function getSingleBook(id) {
  * Imprime en el HTML una lista de la colección/array de books pasada por parametro.
  *
  * @export
- * @param {books[]} booksArr 
+ * @param {books[]} booksArr
  * @modified Modificado para que detecte en que página está, index.html o admin.html, y devuelva la lista con una estructura HTML u otra. Se inicializan los popovers.
  * @author {Nico Fernández}{Ángel Aragón}
  */
 export function printListBooks(booksArr) {
-    if (isAdminPage){
-        tableAdmin.innerHTML = "";
-        booksArr.forEach(book => {
-            tableAdmin.innerHTML += `
+  if (isAdminPage) {
+    tableAdmin.innerHTML = "";
+    booksArr.forEach((book) => {
+      tableAdmin.innerHTML += `
             <tr>
               <td>
                 <button
-                  class="btn btn-outline-primary"
-                  class="btn btn-success"
+                  class="btn btn-outline-primary btn--openModal"
                   data-bs-toggle="popover"
                   data-bs-trigger="hover focus"
                   data-bs-title="Visualizar libro"
                   data-bs-content="Abre una ventana modal para visualizar todos los datos de este libro."
+                  id="get${book.id}"
                 >
                   ${book.id}
                 </button>
@@ -83,39 +95,45 @@ export function printListBooks(booksArr) {
               <td class="text-capitalize">${book.category}</td>
               <td class="d-grid gap-2 d-md-block">
                 <button
-                  class="btn btn-primary"
-                  class="btn btn-success"
+                  class="btn btn-primary btn--openModal"
                   data-bs-toggle="popover"
                   data-bs-placement="left"
                   data-bs-trigger="hover focus"
                   data-bs-title="Editar libro"
                   data-bs-content="Abre una ventana modal para editar este libro."
+                  id="upd${book.id}"
                 >
                   <i class="bi bi-pencil-square text-light"></i>
                 </button>
                 <button
-                  class="btn btn-danger"
-                  class="btn btn-success"
+                  class="btn btn-danger btn--openModal"
                   data-bs-toggle="popover"
                   data-bs-placement="left"
                   data-bs-trigger="hover focus"
                   data-bs-title="Borrar libro"
                   data-bs-content="Abre una ventana modal para borrar este libro de la base de datos."
-                >
+                  id="del${book.id}"
+                  >
                   <i class="bi bi-trash text-light"></i>
                 </button>
               </td>
             </tr>
             `;
-            
-        })
-        initPopovers();
-    } else {
-        grid.innerHTML = "";
-        booksArr.forEach((book) => {
-            grid.insertAdjacentHTML(
-                "beforeend",
-                `<article class="books__card">
+    });
+
+    booksArr.forEach((book) => {
+      document.getElementById("get" + book.id).addEventListener("click", () => {
+        printModalBook(book);
+      });
+    });
+    initPopovers();
+    initModals();
+  } else {
+    grid.innerHTML = "";
+    booksArr.forEach((book) => {
+      grid.insertAdjacentHTML(
+        "beforeend",
+        `<article class="books__card">
                         <img src="./src/images/cover_${book.id}.jpg" alt="Portada ${book.title}"
                             class="books__card__img" onerror="this.onerror=null; this.src='https://placehold.co/600x400';">
                         <div class="books__card__body">
@@ -124,11 +142,10 @@ export function printListBooks(booksArr) {
                             <button class="books__card__body__btn btn"><i class="bi bi-book"></i> Leer Ahora</button>
                         </div>
                     </article>`
-            );
-        });
-    }
+      );
+    });
+  }
 }
-
 
 /**
  * Método que recoge un Get de todos los libros y llama a printListBooks(books) para imprimirlos
@@ -139,8 +156,27 @@ export function printListBooks(booksArr) {
  * @author {Nico Fernández}{Ángel Aragón}
  */
 export async function printAllBooks() {
-    const books = await getAllBooks();
-    printListBooks(books);
+  const books = await getAllBooks();
+  printListBooks(books);
+}
+
+function printModalBook(book) {
+  modalLabel.innerHTML = `<i class="bi bi-book-half"></i> ${book.title}`;
+  modalBody.innerHTML = `<div class="row">
+    <div class="col-md">
+      <img src="../images/cover_${book.id}.jpg" alt="Portada ${book.title}"
+      class="img-fluid" onerror="this.onerror=null; this.src='https://placehold.co/600x400';">
+    </div>
+    <div class="col-md-8">
+      <ul class="list-group">
+        <li class="list-group-item"><div class="fw-bold">Título</div><i class="bi bi-book"></i>${book.title}</li>
+        <li class="list-group-item"><div class="fw-bold">Autor</div><i class="bi bi-person"></i>${book.author}</li>
+        <li class="list-group-item"><div class="fw-bold">Año de publicación</div>${book.publish_year}</li>
+        <li class="list-group-item text-capitalize"><div class="fw-bold">Categoría</div>${book.category}</li>
+        <li class="list-group-item"><div class="fw-bold">Sinopsis</div>${book.synopsis}</li>
+      </ul>
+    </div>
+  </div>`;
 }
 
 printAllBooks();
---
+++ src/utils/modal.js
@@ -0,0 +1,16 @@
+/**
+ * Inicializa el evento para abrir ventana modal
+ * @author {Ángel Aragón}
+ * @export
+ */
+export function initModals() {
+    const modalButtons = document.querySelectorAll(".btn--openModal");
+    console.log(modalButtons);
+    
+    modalButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            const myModal = new bootstrap.Modal(document.getElementById("modal"));
+            myModal.show();
+          });
+    });
+}
